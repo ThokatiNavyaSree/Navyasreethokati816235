b505183353afe47f303f22c0a98e552b09b1fb78
ThokatiNavyaSree <100119010+ThokatiNavyaSree@users.noreply.github.com>
2022-03-07 20:25:29 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,25 @@
+insert into employee(id, name, last_name) 
+values(1,'sree','navya');
+insert into employee (id, name, last_name) 
+values(2,'sreeya' ,'thokati' );
+insert into employee(id, name, last_name ) 
+values(3,'ninja', 'hitman');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
